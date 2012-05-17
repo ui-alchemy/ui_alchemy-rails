4dde2b3fc39369f6f1567d8bbdbe55dadda31b18
Eric D Helms <ehelms@redhat.com>
2012-05-17 16:15:17 -0400

    Adds updated password reset screen with translations and formatting.

--- rails/helpers/gettext_translations.rb
+++ rails/helpers/gettext_translations.rb
@@ -5,6 +5,17 @@ module ConvergeUi::GettextTranslations
     :forgot_username_password => _("Forgot username or password?"),
     :username                 => _("Username"),
     :password                 => _("Password"),
-    :login                    => _("Login")
+    :login                    => _("Login"),
+    :login_information        => _("Login Information"),
+    :remembered_login         => _('I remembered my login credentials.'),
+    :password_unknown         => _('Password Unknown'),
+    :email_address            => _('Email Address'),
+    :send_login               => _('Send Login'),
+    :return_to_login          => _("Return to log in."),
+    :password_unknown         => _("Password Unknown"),
+    :password_unknown_info    => _('If you do not know your password, enter your login and email address below and instructions on resetting your password will be sent to you.'),
+    :reset_password           => _('Reset Password'),
+    :login_unknown            => _('Login Unknown'),
+    :login_unknown_info       => _("If you do not know your login, please enter your email address below and your login will be sent to you.")
   }
 end
