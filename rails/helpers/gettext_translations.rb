a8a3ff1dbbfb59182518d34dea3790db4e191076
Eric D Helms <ehelms@redhat.com>
2012-05-24 12:16:11 -0400

    Touch-ups to the login screen.

--- rails/helpers/gettext_translations.rb
+++ rails/helpers/gettext_translations.rb
@@ -1,8 +1,8 @@
 module ConvergeUi::GettextTranslations
   TRANSLATIONS = {
-    :show_password            => _("Show Password"),
+    :show_password            => _("Show My Password"),
     :example_logo             => _("Logo"),
-    :forgot_username_password => _("Forgot username or password?"),
+    :forgot_username_password => _("Forgot Username or Password?"),
     :username                 => _("Username"),
     :password                 => _("Password"),
     :login                    => _("Login"),
