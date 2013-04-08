9164b1df4995b729757c757254349f85fe7a0482
Eric D Helms <ehelms@redhat.com>
2013-04-08 12:02:42 -0400

    Updates to move references to the alchemy component.

--- lib/ui_alchemy/rails/engine.rb
+++ lib/ui_alchemy/rails/engine.rb
@@ -2,5 +2,9 @@ module UIAlchemy
   class Engine < ::Rails::Engine
     isolate_namespace UIAlchemy
 
+    initializer "ui_alchemy.assets.paths" do |app|
+      app.config.assets.paths << "#{self.root}/vendor/assets"
+      app.config.assets.paths << "#{self.root}/vendor/assets/ui_alchemy"
+    end
   end
 end
