56d9c2d21cea25d2aef0ace27450ed387d97b209
Eric D. Helms <ericdhelms@gmail.com>
2013-06-15 14:05:00 -0400

    Alchemy-header update.

--- vendor/assets/ui_alchemy/alchemy-header/alchemy-header.js
+++ vendor/assets/ui_alchemy/alchemy-header/alchemy-header.js
@@ -1,57 +1,122 @@
 angular.module("alch-templates").run(["$templateCache", function($templateCache) {
+
   $templateCache.put("component/templates/dropdown.html",
-    "<ul class=\"dropdown\" " +
-    "    ng-class=\"{ 'dropdown-right' : isRight(dropdown.direction), 'dropdown-active' : dropdown.show }\">" +
-    "  <li ng-repeat=\"item in dropdown\"" +
-    "      ng-mouseenter=\"set_hover(item, true)\"" +
-    "      ng-mouseleave=\"set_hover(item, false)\">" +
-    "  " +
-    "    <a class=\"dropdown-menu-item-link\" href=\"{{ item.url }}\">" +
-    "      {{ item.display }}" +
-    "      <i class=\"right_arrow_icon-grey flyout-indicator\" ng-show=\"item.type=='flyout'\"></i>" +
-    "    </a>" +
-    "    <ul alch-flyout=\"item.items\" ng-show=\"flyout.show\" ng-class=\"{ 'dropdown-active' : dropdown.show }\"></ul>" +
-    "  </li>" +
-    "</ul>" +
-    "");
-}]);
+    "<ul class=\"dropdown\"\n" +
+    "    ng-class=\"{ 'dropdown-right' : isRight(dropdown.direction), 'dropdown-active' : dropdown.show }\">\n" +
+    "  <li class=\"dropdown-item\"\n" +
+    "      ng-repeat=\"item in dropdown\"\n" +
+    "      ng-mouseenter=\"setHover(item, true)\"\n" +
+    "      ng-mouseleave=\"setHover(item, false)\">\n" +
+    "\n" +
+    "    <a class=\"dropdown-item-link\"\n" +
+    "       href=\"{{ item.url }}\"\n" +
+    "       ng-class=\"{'menu-anchor' : item.type, 'dropdown-item-link-active' : item.active }\">\n" +
+    "      <span class=\"fl\">{{ item.display }}</span>\n" +
+    "      <i class=\"icon-caret-right flyout-indicator\" ng-show=\"item.type=='flyout'\"></i>\n" +
+    "    </a>\n" +
+    "    <ul alch-flyout=\"item.items\"\n" +
+    "        ng-show=\"flyout.show\"\n" +
+    "        ng-class=\"{ 'dropdown-active' : dropdown.show }\">\n" +
+    "    </ul>\n" +
+    "  </li>\n" +
+    "</ul>\n"
+  );
 
-angular.module("alch-templates").run(["$templateCache", function($templateCache) {
   $templateCache.put("component/templates/flyout.html",
-    "<ul class=\"flyout\">" +
-    "  <li ng-repeat=\"item in flyout\"" +
-    "      ng-class=\"{ 'dropdown-highlight' : item.active }\"" +
-    "      ng-mouseenter=\"set_hover(item, true)\"" +
-    "      ng-mouseleave=\"set_hover(item, false)\">" +
-    "  " +
-    "    <a href=\"{{ item.url }}\">{{ item.display }}</a>" +
-    "  </li>" +
-    "</ul>" +
-    "");
+    "<ul class=\"flyout\">\n" +
+    "  <li class=\"flyout-item\"\n" +
+    "      ng-repeat=\"item in flyout\"\n" +
+    "      ng-class=\"{ 'dropdown-highlight' : item.active }\"\n" +
+    "      ng-mouseenter=\"setHover(item, true)\"\n" +
+    "      ng-mouseleave=\"setHover(item, false)\">\n" +
+    "  \n" +
+    "    <a class=\"flyout-item-link\" href=\"{{ item.url }}\">{{ item.display }}</a>\n" +
+    "  </li>\n" +
+    "</ul>\n"
+  );
+
+  $templateCache.put("component/templates/menu.html",
+    "<nav ng-class=\"menu.location\">\n" +
+    "  <ul class=\"menu-container\">\n" +
+    "\n" +
+    "    <li class=\"menu-item\"\n" +
+    "        ng-repeat=\"item in menu.items\"\n" +
+    "        ng-mouseenter=\"handleHover(item, true)\"\n" +
+    "        ng-mouseleave=\"handleHover(item, false)\">\n" +
+    "\n" +
+    "        <a href=\"{{ item.url }}\"\n" +
+    "           ng-class=\"{ 'active-item' : item.active, 'menu-anchor' : item.type }\"\n" +
+    "           class=\"menu-item-link\"> \n" +
+    "          <span ng-bind-html-unsafe=\"item.display\"></span>\n" +
+    "         <i class=\"icon-caret-down\" ng-show=\"item.type == 'dropdown'\"></i>\n" +
+    "        </a>\n" +
+    "        <ul alch-dropdown=\"item.items\"></ul>\n" +
+    "    </li>\n" +
+    "  </ul>\n" +
+    "</nav>\n"
+  );
+
 }]);
 
-angular.module("alch-templates").run(["$templateCache", function($templateCache) {
-  $templateCache.put("component/templates/menu.html",
-    "<nav ng-class=\"menu.location\">" +
-    "  <ul class=\"menu-container\">" +
-    "    <li class=\"menu-item\"" +
-    "        ng-repeat=\"item in menu.items\"" +
-    "        ng-mouseenter=\"handle_hover(item, true)\"" +
-    "        ng-mouseleave=\"handle_hover(item, false)\">" +
-    "        " +
-    "        <a href=\"{{ item.url }}\" " +
-    "           ng-class=\"{ 'active-item' : item.active }\"" +
-    "           class=\"menu-item-link\">" +
-    "        " +
-    "          {{ item.display }}" +
-    "          <i class=\"down_arrow_icon-white\" ng-show=\"item.type == 'dropdown'\"></i>" +
-    "        </a>" +
-    "        <ul alch-dropdown=\"item.items\"></ul>" +
-    "    </li>" +
-    "  </ul>" +
-    "</nav>" +
-    "");
-}]);
+'use strict';
+
+angular.module('alchemy').directive('alchDropdown', function(){
+    return {
+        restrict: 'EA',
+        transclude: true,
+        replace: true,
+        scope: {
+            'dropdown' : '=alchDropdown'
+        },
+        templateUrl: 'component/templates/dropdown.html',
+
+        controller: ['$scope', function($scope) {
+            $scope.setHover = function(item, mousein) {
+                if (mousein) {
+                    item.active = true;
+
+                    if (item.type === 'flyout') {
+                        $scope.flyout = item.items;
+                        $scope.flyout.show = true;
+                    }
+                } else {
+                    if ($scope.flyout) {
+                        $scope.flyout.show = false;
+                    }
+                    item.active = false;
+                }
+            };
+
+            $scope.isRight = function(direction) {
+                return direction === 'right';
+            };
+        }]
+    };
+});
+
+'use strict';
+
+angular.module('alchemy').directive('alchFlyout', function(){
+    return {
+        restrict: 'EA',
+        transclude: true,
+        replace: true,
+        scope: {
+            'flyout' : '=alchFlyout'
+        },
+        templateUrl: 'component/templates/flyout.html',
+
+        controller: ['$scope', function($scope) {
+            $scope.setHover = function(item, mousein) {
+                if (mousein) {
+                    item.active = true;
+                } else {
+                    item.active = false;
+                }
+            };
+        }]
+    };
+});
 
 'use strict';
 
@@ -65,12 +130,11 @@ angular.module('alchemy').directive('alchMenu', ['$window', function($window){
             'compact' : '@'
         },
         templateUrl: 'component/templates/menu.html',
-
         controller: ['$scope', function($scope) {
             $scope.dropdown = {};
 
-            $scope.handle_hover = function(item, mousein){
-                if( item.type === 'dropdown' && mousein ){
+            $scope.handleHover = function(item, mousein) {
+                if (item.type === 'dropdown' && mousein) {
                     item.active = true;
                     $scope.dropdown = item.items;
                     $scope.dropdown.show = true;
@@ -78,26 +142,25 @@ angular.module('alchemy').directive('alchMenu', ['$window', function($window){
                 } else {
                     $scope.dropdown.show = false;
 
-                    if( item !== $scope.menu.active_item ){
+                    if (item !== $scope.menu.activeItem) {
                         item.active = false;
                     }
                 }
             };
 
         }],
+        link: function(scope, element, attrs) {
+            var elementOriginalOffset;
 
-        link: function(scope, element, attrs){
-            var element_original_offset;
+            if (attrs.compact !== undefined) {
+                elementOriginalOffset = $(element).offset().top;
 
-            if( attrs.compact !== undefined ){
-                element_original_offset = $(element).offset().top;
+                angular.element($window).bind('scroll', function() {
+                    var windowScrollTop = $($window).scrollTop();
 
-                angular.element($window).bind('scroll', function(){
-                    var window_scroll_top = $($window).scrollTop();
-
-                    if( window_scroll_top > element_original_offset + 2 ){
+                    if (windowScrollTop > elementOriginalOffset + 2) {
                         element.parent().addClass('compact');
-                    } else if( window_scroll_top < element_original_offset ) {
+                    } else if (windowScrollTop < elementOriginalOffset) {
                         element.parent().removeClass('compact');
                     }
                  });
@@ -105,59 +168,3 @@ angular.module('alchemy').directive('alchMenu', ['$window', function($window){
         }
     };
 }]);
-
-angular.module('alchemy').directive('alchDropdown', function(){
-    return {
-        restrict: 'EA',
-        transclude: true,
-        replace: true,
-        scope: {
-            'dropdown' : '=alchDropdown'
-        },
-        templateUrl: 'component/templates/dropdown.html',
-
-        controller: ['$scope', function($scope){
-            $scope.set_hover = function(item, mousein){
-                if( mousein ){
-                    item.active = true;
-
-                    if( item.type === 'flyout' ){
-                        $scope.flyout = item.items;
-                        $scope.flyout.show = true;
-                    }
-                } else {
-                    if( $scope.flyout ){
-                        $scope.flyout.show = false;
-                    }
-                    item.active = false;
-                }
-            };
-
-            $scope.isRight = function(direction){
-                return direction === 'right';
-            };
-        }]
-    };
-});
-
-angular.module('alchemy').directive('alchFlyout', function(){
-    return {
-        restrict: 'EA',
-        transclude: true,
-        replace: true,
-        scope: {
-            'flyout' : '=alchFlyout'
-        },
-        templateUrl: 'component/templates/flyout.html',
-
-        controller: ['$scope', function($scope){
-            $scope.set_hover = function(item, mousein){
-                if( mousein ){
-                    item.active = true;
-                } else {
-                    item.active = false;
-                }
-            };
-        }]
-    };
-});
